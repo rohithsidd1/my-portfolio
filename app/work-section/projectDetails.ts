b8b77058dd2667961845aa7642c5c8eee46d0089
rohith siddanathi <rohithsiddanathi.ios@gmail.com>
2025-03-03 16:10:01 +0530

    new

--- app/work-section/projectDetails.ts
+++ app/work-section/projectDetails.ts
@@ -9,164 +9,129 @@ export type ProjectProps = {
   available: boolean;
 };
 
-export const devProjects = [
+export const devProjects: ProjectProps[] = [
   {
     id: 0,
     name: "SubsTrack",
     description:
-      "SubsTrack is a minimalist mobile application designed to track all your Subscriptions in one place and optimize your savings.",
+      "SubsTrack is a minimalist mobile application designed to track all your subscriptions in one place and optimize your savings.",
     technologies: ["SwiftUI", "MVVM", "User Defaults"],
     github: "https://github.com/rohithsidd1/SubsTrack",
     demo: "https://www.behance.net/gallery/219256307/SubsTrack",
-    image: require(".//../../public/projects/st.png"),
+    image: require("../../public/projects/st.png"),
     available: true,
   },
   {
     id: 1,
     name: "NewsSense",
     description:
-      "A iOS app that delivers a dynamic and personalized news-reading experience with the user's location.",
+      "An iOS app that delivers a dynamic and personalized news-reading experience based on the user's location.",
     technologies: ["SwiftUI", "RESTful APIs", "GeoLocation API"],
     github: "https://github.com/rohithsidd1/NewsSense",
     demo: "https://www.behance.net/gallery/219263573/NewsSense",
-    image: require(".//../../public/projects/ns.png"),
+    image: require("../../public/projects/ns.png"),
     available: true,
   },
   {
     id: 2,
     name: "WorkFlow",
     description:
-      "WorkFlow is a powerful iOS app designed to streamline productivity and task management of your daily work stream.",
+      "WorkFlow is a powerful iOS app designed to streamline productivity and task management.",
     technologies: ["SwiftUI", "Core Data", "Core Animations"],
     github: "https://github.com/rohithsidd1/WorkFlow",
     demo: "https://www.behance.net/gallery/219262465/WorkFlow",
-    image: require(".//../../public/projects/wf.png"),
+    image: require("../../public/projects/wf.png"),
     available: true,
   },
   {
     id: 3,
     name: "Fynd!",
     description:
-      "App for everything happening around you, Redefined. Fynd helps not just find a place but also finds you a buddy that shares similar interest.",
+      "An app for discovering events around you and connecting with like-minded people.",
     technologies: ["SwiftUI", "Google APIs", "MapKit"],
     github: "https://github.com/rohithsidd1/Fynd",
     demo: "https://www.behance.net/gallery/219264729/Fynd-For-You",
-    image: require(".//../../public/projects/fy.png"),
+    image: require("../../public/projects/fy.png"),
     available: true,
   },
   {
     id: 4,
     name: "ReNoteAI",
     description:
-      "ReNoteAI is AI-based Document scanner app with minimal design and all sleek features like OCR, Chatbot, Cloud integrations and Personalized templates.",
+      "An AI-powered document scanner with features like OCR, chatbot, cloud integration, and personalized templates.",
     technologies: ["SwiftUI", "Vision", "SSO", "RESTful APIs"],
     github: "https://github.com/rohithsidd1/ReNoteAI",
     demo: "https://www.behance.net/gallery/219266041/ReNoteAI",
-    image: require(".//../../public/projects/re.png"),
+    image: require("../../public/projects/re.png"),
     available: true,
   },
-  // {
-  //   id: 4,
-  //   name: "SkyWatch",
-  //   description:
-  //     "SkyWatch is a convenient and user-friendly tool that allows you to quickly and easily check the current weather and forecast for the next 2 days in any city.",
-  //   technologies: ["React", "CSS", "Chart.js"],
-  //   github: "https://github.com/victorcodess/weather-forecast-website",
-  //   demo: "https://sky-watch.vercel.app/",
-  //   image: require(".//../../public/projects/skywatch-flip.png"),
-  //   available: true,
-  // },
-  // {
-  //   id: 4,
-  //   name: "Alpaca Image Generator",
-  //   description:
-  //     "An image generator website that allows users to generate, combine, and download images.",
-  //   technologies: ["React", "CSS", "Merge Images"],
-  //   github: "https://github.com/victorcodess/alpaca-image-generator",
-  //   demo: "http://alpaca-image-generator-beta.vercel.app",
-  //   image: require(".//../../public/projects/alpaca-flip.png"),
-  //   available: true,
-  // },
-
   {
     id: 5,
     name: "Match Cards",
     description:
-      "MatchCards is a fun and engaging iOS game designed to enhance concentration while providing an enjoyable experience.",
+      "A fun and engaging iOS game designed to enhance concentration while providing an enjoyable experience.",
     technologies: ["SwiftUI", "Core Animations", "User Defaults"],
     github: "https://github.com/rohithsidd1/MatchCards",
     demo: "https://www.behance.net/gallery/219263829/MatchCards",
-    image: require(".//../../public/projects/mc.png"),
+    image: require("../../public/projects/mc.png"),
     available: true,
   },
   {
     id: 6,
     name: "Brad.ai",
     description:
-      "Brad.ai is an intelligent, multilingual chatbot application designed to provide seamless interactions across various use cases.",
+      "An intelligent, multilingual chatbot application designed for seamless interactions across various use cases.",
     technologies: ["SwiftUI", "MVVM", "RESTful APIs"],
     github: "https://github.com/rohithsidd1/brad.ai",
     demo: "https://www.behance.net/gallery/220538585/Bradai",
-    image: require(".//../../public/projects/br.png"),
+    image: require("../../public/projects/br.png"),
     available: true,
   },
-  // {
-  //   id: 7,
-  //   name: "MLSC.ng",
-  //   description:
-  //     "This is platform for Microsoft Learn Student Ambassadors to shorten links, append their sharing IDs and generate event certificates.",
-  //   technologies: ["Next.js", "Next Auth", "Tailwind CSS"],
-  //   github: "https://github.com/msp-nigeria/mlsc.ng-iOS",
-  //   demo: "",
-  //   image: require(".//../../public/projects/mlsc.png"),
-  //   available: false,
-  // },
 ];
 
-export const designProjects = [
+export const designProjects: ProjectProps[] = [
   {
     id: 1,
-    name: "Hebron Statup Lab Website",
+    name: "Hebron Startup Lab Website",
     description:
-      "SkyWatch is a convenient and user-friendly tool that allows you to quickly and easily check the current.",
+      "A convenient and user-friendly tool to quickly and easily check project details.",
     technologies: ["UX Research", "UI Design", "Prototyping"],
     github: "",
     demo: "",
-    image: "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Fhsl.webp&w=1920&q=75",
+    image: require("../../public/projects/hsl.webp"),
     available: false,
   },
   {
     id: 2,
     name: "RAGS Scrubs Website",
     description:
-      "An image generator website that allows users to generate, combine, and download images.",
+      "An intuitive website for generating, combining, and downloading custom images.",
     technologies: ["UX Research", "UI Design", "Prototyping"],
     github: "",
     demo: "",
-    image: "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Frags.webp&w=1920&q=75",
+    image: require("../../public/projects/rags.webp"),
     available: false,
   },
   {
     id: 3,
     name: "Crown Branding Agency Website",
     description:
-      "A website that reduces the length of your URL using Bit.ly's API",
+      "A branding website that shortens URLs using Bit.ly's API.",
     technologies: ["UX Research", "UI Design", "Prototyping"],
     github: "",
     demo: "",
-    image:
-      "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Fcrown.webp&w=1920&q=75",
+    image: require("../../public/projects/crown.webp"),
     available: false,
   },
   {
     id: 4,
     name: "Titi Mobile App",
     description:
-      "TMTM helps you find people who are headed to the same location as you, so you can share a ride and split the cost with them.",
+      "An app to help users find carpool partners headed in the same direction.",
     technologies: ["UX Research", "UI Design", "Prototyping"],
     github: "",
     demo: "",
-    image: "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Ftiti.webp&w=1920&q=75",
+    image: require("../../public/projects/titi.webp"),
     available: false,
   },
 ];
