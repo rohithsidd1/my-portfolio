b0af4703ee352205774901e0e5161460d8c4404e
rohith siddanathi <rohithsiddanathi.ios@gmail.com>
2025-03-03 17:01:46 +0530

    prooo

--- app/work-section/projectDetails.ts
+++ app/work-section/projectDetails.ts
@@ -65,29 +65,6 @@ export const devProjects = [
     image: require(".//../../public/projects/re.png"),
     available: true,
   },
-  // {
-  //   id: 4,
-  //   name: "SkyWatch",
-  //   description:
-  //     "SkyWatch is a convenient and user-friendly tool that allows you to quickly and easily check the current weather and forecast for the next 2 days in any city.",
-  //   technologies: ["React", "CSS", "Chart.js"],
-  //   github: "https://github.com/victorcodess/weather-forecast-website",
-  //   demo: "https://sky-watch.vercel.app/",
-  //   image: require(".//../../public/projects/skywatch-flip.png"),
-  //   available: true,
-  // },
-  // {
-  //   id: 4,
-  //   name: "Alpaca Image Generator",
-  //   description:
-  //     "An image generator website that allows users to generate, combine, and download images.",
-  //   technologies: ["React", "CSS", "Merge Images"],
-  //   github: "https://github.com/victorcodess/alpaca-image-generator",
-  //   demo: "http://alpaca-image-generator-beta.vercel.app",
-  //   image: require(".//../../public/projects/alpaca-flip.png"),
-  //   available: true,
-  // },
-
   {
     id: 5,
     name: "Match Cards",
@@ -99,28 +76,17 @@ export const devProjects = [
     image: require(".//../../public/projects/mc.png"),
     available: true,
   },
-  // {
-  //   id: 6,
-  //   name: "Carpooling Service",
-  //   description:
-  //     "TMTM helps Covenant University students find fellow students who are headed to the same location, so they can share a ride and split the cost.",
-  //   technologies: ["Material UI", "React", "Formik"],
-  //   github: "https://github.com/victorcodess/carpooling-service",
-  //   demo: "",
-  //   image: require(".//../../public/projects/carpool-new.webp"),
-  //   available: false,
-  // },
-  // {
-  //   id: 7,
-  //   name: "MLSC.ng",
-  //   description:
-  //     "This is platform for Microsoft Learn Student Ambassadors to shorten links, append their sharing IDs and generate event certificates.",
-  //   technologies: ["Next.js", "Next Auth", "Tailwind CSS"],
-  //   github: "https://github.com/msp-nigeria/mlsc.ng-iOS",
-  //   demo: "",
-  //   image: require(".//../../public/projects/mlsc.png"),
-  //   available: false,
-  // },
+  {
+    id: 6,
+    name: "TaskMaster",
+    description:
+      "Brad.ai is an intelligent, multilingual chatbot application designed to provide seamless interactions across various use cases.",
+    technologies: ["SwiftUI", "MVC", "RESTful APIs"],
+    github: "https://github.com/rohithsidd1/Brad.ai",
+    demo: "https://www.behance.net/gallery/220538585/Bradai",
+    image: require(".//../../public/projects/br.png"),
+    available: true,
+  },
 ];
 
 export const designProjects = [
